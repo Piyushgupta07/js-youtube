aa79e03930516a8c7138fd6ef31364f36178e1b6
Piyushgupta07 <109663813+Piyushgupta07@users.noreply.github.com>
2025-07-10 19:40:46 +0000

    conversion

--- Js_basics/03_Conversion.js
+++ Js_basics/03_Conversion.js
@@ -1,3 +1,16 @@
-console.log("Piyush");
-let age = 33;
-console.log(typeof age);
+// console.log("Piyush");
+// let age = 33;
+// console.log(typeof age);
+// let score = "33abc";
+// let onlyNumbers = Number(score);
+// console.log(typeof onlyNumbers);
+// console.log(onlyNumbers);
+
+// 33 => number
+//"33" => number
+//"33abc" => NaN
+// true => 1, false => 0
+
+let isLoggedin = 1;
+let booleanisLoggedIn = Boolean(isLoggedin);
+console.log(booleanisLoggedIn);
